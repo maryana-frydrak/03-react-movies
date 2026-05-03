37423918a451ef2f6d3b25aaf284df6bb0c809ef
maryana-frydrak <maryanafrydrak@gmail.com>
2026-05-03 22:52:29 +0300

    finished 3 HW

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,64 @@
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "../../assets/vite.svg";
-import heroImg from "../../assets/hero.png";
-import "./App.module.css";
+import { Toaster, toast } from "react-hot-toast";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { fetchMovies } from "../../services/movieService";
+import { type Movie } from "../../types/movie";
+import styles from "./App.module.css";
+import { MovieGrid } from "../MovieGrid/MovieGrid";
+import { Loader } from "../Loader/Loader";
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
+import { MovieModal } from "../MovieModal/MovieModal";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  return <></>;
+  const openModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+  };
+
+  const handleSearch = async (query: string) => {
+    setMovies([]);
+    setError(false);
+    setLoading(true);
+
+    try {
+      const results = await fetchMovies(query);
+
+      if (results.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+
+      setMovies(results);
+    } catch (error) {
+      console.error(error);
+      setError(true);
+      toast.error("Something went wrong. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={styles.app}>
+      <Toaster position="top-right" />
+      <SearchBar onSubmit={handleSearch} />
+
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {movies.length > 0 && <MovieGrid movies={movies} onSelect={openModal} />}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
+      )}
+    </div>
+  );
 }
 
 export default App;
